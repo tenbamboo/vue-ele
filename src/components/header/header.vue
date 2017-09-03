98df9e86a160254d8304fa9aca68fb3c5a917856
haze.liu <liu19891014@163.com>
2017-09-03 16:48:14 +0800

    1

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -19,30 +19,58 @@
 				</span>
 			</div>
 			</div> 
-			<div v-if="seller.supports" class="support-count">
+			<div v-if="seller.supports" class="support-count" @click="showDetail">
 				<span class="count">{{seller.supports.length}}个</span>
 				<i class="icon-keyboard_arrow_right"></i>
 			</div>
 		</div>
-		<div class="bullentin-wrapper">
+		<div class="bullentin-wrapper" @click="showDetail">
 			<span class="bulletin-title"></span> <span class="bulletin-text">{{seller.bulletin}}</span>
 			<i class="icon-keyboard_arrow_right"></i>
 		</div>
 		<div class="background">
 			<img :src="seller.avatar" width="100%" height="100%" />
 		</div>
+		<div class="detail" v-show="detailShow">
+			<div class=" clearfix detail-wrapper">
+				<div class="detail-main">
+					<h1 class="name">{{seller.name}}</h1>
+					<div class="star-wrapper">
+						<star :size="48" :score="seller.score"></star>
+					</div>
+				</div>
+			</div>
+			<div class="detail-close">
+				<i class="icon-close"></i>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+	import star from '../../components/star/star.vue';
+
 	export default{
 		props: {
 			seller: {
 				type: Object
 			}
 		},
+		data(){
+			return {
+				detailShow: false
+			};
+		},
+		methods:{
+			showDetail(){
+				this.detailShow = true;
+			}
+		},
 		created(){
 			this.classMap = ['decrease','discount','special','invoice','guarantee'];
+		},
+		components:{
+			star
 		}
 
 	};
@@ -53,6 +81,7 @@
 	.header
 		position:relative
 		color:#fff
+		overflow:hidden
 		background:rgba(7,17,27,0.5)
 		.content-wrapper
 			position:relative
@@ -161,6 +190,40 @@
 			height:100%
 			z-index:-1
 			filter:blur(10px)
+		.detail
+			position:fixed
+			z-index:100
+			width:100%
+			height:100%
+			overflow:auto
+			background:rgba(7,17,27,0.8)
+			top:0
+			left:0
+			.detail-wrapper
+				min-height:100%
+				width:100%
+				.detail-main
+					margin-top:64px
+					padding-bottom:64px
+					.name
+						line-height:16px
+						text-align:center
+						font-size16px
+						font-weight:700
+					.star-wrapper
+						margin-top:18px
+						padding:2px 0
+						text-align:center
+			.detail-close
+				position:relative
+				width:32px
+				height:32px
+				margin:-64px auto
+				clear:both
+				font-size:32px
+
+
+
 
 
 
