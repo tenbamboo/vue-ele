d285acbabff58e69c549b3e66982219a8fa888e8
haze.liu <liu19891014@163.com>
2017-09-05 22:18:32 +0800

    1

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,16 @@
 <template>
 	<div class="goods">
-		<div class="menu-wrapper"></div>
+		<div class="menu-wrapper">
+			<ul>
+				<li v-for="(item,index) in goods" class="menu-item" >
+					<span class="text border-1px">
+						<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+					</span>
+				</li>
+
+			</ul>
+
+		</div>
 		<div class="foods-wrapper"></div>
 	</div>
 </template>
@@ -26,11 +36,14 @@
 		        	console.log(this.goods);
 		        }
 		    });
+			this.classMap = ['decrease','discount','special','invoice','guarantee'];
 		}
 	};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+@import '../../common/stylus/mixin.styl'
+
 .goods
 	position:absolute
 	width:100%
@@ -42,6 +55,36 @@
 		flex: 0 0 80px
 		width:80px
 		background:#f3f5f7
+		.menu-item
+			display:table
+			height:54px
+			width:56px
+			line-height:14px
+			padding:0 12px
+			.icon
+				vertical-align:top
+				display:inline-block
+				width:12px
+				height:12px
+				margin-right:2px
+				background-size:12px 12px
+				background-repeat:no-repeat
+				&.decrease
+					bg-image('decrease_3')
+				&.discount
+					bg-image('discount_3')
+				&.guarantee
+					bg-image('guarantee_3')
+				&.invoice
+					bg-image('invoice_3')
+				&.special
+					bg-image('special_3')
+			.text
+				font-size:12px
+				display:table-cell
+				width:56px
+				vertical-align:middle
+				border-1px(rgba(7,17,27,0.1))
 	.foods-wrapper
 		flex : 1
 
