073c72578619db1f7adda3fd04b96f05470d4ea4
haze.liu <liu19891014@163.com>
2017-09-16 12:41:08 +0800

    1

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="ratingselect">
 		<div class="rating-type border-1px">
-			<span>{{desc.all}}</span>
-			<span>{{desc.positive}}</span>
-			<span>{{desc.negative}}</span>
+			<span @click="select(2)" class="block positive" :class="{'active':selectType==2}">{{desc.all}}<span class="count">{{ratings.length}}</span></span>
+			<span @click="select(0)" class="block positive"  :class="{'active':selectType==0}">{{desc.positive}}<span class="count">{{positives.length}}</span></span>
+			<span @click="select(1)" class="block negative"  :class="{'active':selectType==1}">{{desc.negative}}<span class="count">{{negatives.length}}</span></span>
 		</div>
-		<div class="switch">
+		<div @click="toggleContent" class="switch" :class="{'on':onlyContent}">
 			<i class="icon-check_circle"></i>
 			<span class="text">只看有内容的评价</span>
 		</div>
@@ -15,8 +15,8 @@
 </template>
 
 <script>
-// const POSITIVE = 0;
-// const NEGATIVE = 1;
+const POSITIVE = 0;
+const NEGATIVE = 1;
 const ALL = 2;
 export default {
 	props:{
@@ -44,6 +44,26 @@ export default {
 				};
 			}
 		}
+	},
+	computed:{
+		positives(){
+			return this.ratings.filter((rating) => {
+				return rating.rateType === POSITIVE;
+			});
+		},
+		negatives(){
+			return this.ratings.filter((rating) => {
+				return rating.rateType === NEGATIVE;
+			});
+		}
+	},
+	methods:{
+		select(type) {
+	        this.$emit('select', type);
+	      },
+      	toggleContent() {
+	        this.$emit('toggle');
+	    }
 	}
 };
 </script>
@@ -52,8 +72,52 @@ export default {
 @import '../../common/stylus/mixin.styl'
 	.ratingselect
 		.rating-type
-			margin:18x 0
+			margin:18px 0
 			padding:0 18px
 			border-1px(rgba(7,17,27,0.1))
+			font-size:0
+			.block
+				padding:8px
+				display:inline-block
+				margin-right:8px
+				border-radius:1px
+				color:rgb(77,87,93)
+				font-size:12px
+				line-height:16px
+				&.active
+					color:#fff
+				.count
+					font-size:8px
+					margin-left:2px
+				&.positive
+					background:rgba(0,160,220,0.2)
+					&.active
+						background:rgba(0,160,220,1)
+				&.negative
+					background:rgba(77,85,93,0.2)
+					&.active
+						background:rgba(77,85,93,1)
+		.switch
+			padding:12px 18px
+			line-height:24px
+			color:rgb(147,153,159)
+			border-bottom:1px solid rgba(7,17,27,0.1)
+			font-size:0
+			.icon-check_circle
+				display:inline-block
+				vertical-align:top
+				font-size:24px
+				margin-right:4px
+			.text
+				display:inline-block
+				vertical-align:top
+				font-size:12px
+			&.on
+				.icon-check_circle
+					color:#00c850
+
+
+
+
 
 </style>
